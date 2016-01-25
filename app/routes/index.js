499dc56c0ca6f39ccddca59aba24b4bd54d6445d
Mongui <ababa@thisisnotmyrealemail.com>
2016-01-25 08:31:11 +0000

    Added some comments to routes/index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,14 +6,17 @@ module.exports = function (app, db) {
         res.sendFile(process.cwd() + '/public/index.html');
     });
     
+    // Timestamp Microservice.
     var Timestamp = require(process.cwd() + '/app/controllers/timestamp.js');
     var timestamp = new Timestamp(db);
     app.route('/projects/timestamp').get(timestamp.get);
     
+    // Request Header Parser Microservice.
     var Whoami = require(process.cwd() + '/app/controllers/whoami.js');
     var whoami = new Whoami(db);
     app.route('/projects/whoami').get(whoami.get);
     
+    // URL Shortener Microservice.
     var Shurli = require(process.cwd() + '/app/controllers/shurli.js');
     var shurli = new Shurli(db);
     app.route('/projects/shurli/new/*').get(shurli.new);
